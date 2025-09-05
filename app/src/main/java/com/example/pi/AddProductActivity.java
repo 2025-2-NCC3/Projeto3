9aadcc32062dd93c27eead1bf63a5be6cc7d977a
Yanko Lee <24026751@fecap.br>
2025-09-04 22:16:11 -0300

    tela de adição de produto(n funcional)

---
+++ app/src/main/java/com/example/pi/AddProductActivity.java
@@ -0,0 +1,12 @@
+package com.example.pi;
+
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+public class AddProductActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cardapio_admin); // Layout da primeira tela
+    }}
