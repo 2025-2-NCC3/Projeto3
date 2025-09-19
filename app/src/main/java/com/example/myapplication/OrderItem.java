081f1866e24640ee82b1818c760bf7aba2073c58
Bruno Eduardo da Silva Pinho <24026660@fecap.br>
2025-09-18 22:22:28 -0300

    Arrumados erros na criação de pedido

--- app/src/main/java/com/example/myapplication/OrderItem.java
+++ app/src/main/java/com/example/myapplication/OrderItem.java
@@ -2,19 +2,19 @@ package com.example.myapplication;
 
 //Aqui é o modelo de cada item do pedido
 public class OrderItem {
-    private String productId;
+    private int productId;
     private String productName;
     private int quantity;
     private double price;
 
-    public OrderItem(String productId, String productName, int quantity, double price) {
+    public OrderItem(int productId, String productName, int quantity, double price) {
         this.productId = productId;
         this.productName = productName;
         this.quantity = quantity;
         this.price = price;
     }
 
-    public String getProductId() { return productId; }
+    public int getProductId() { return productId; }
     public String getProductName() { return productName; }
     public int getQuantity() { return quantity; }
     public double getPrice() { return price; }
