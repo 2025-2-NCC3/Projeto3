6e92278061a15c5acbb041f0f5ecb7dfa0f5b7bf
Bruno Eduardo <brunoedusp303@gmail.com>
2025-10-02 16:28:15 -0300

    Teste tela perfil

--- src/Entrega1/frontend/app/src/main/java/com/example/myapplication/PerfilActivity.java
+++ src/Entrega1/frontend/app/src/main/java/com/example/myapplication/PerfilActivity.java
@@ -1,15 +1,24 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PerfilActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
 
-        
+        TextView nomeUsuario = findViewById(R.id.nomeUsuario);
+        TextView emailUsuario = findViewById(R.id.emailUsuario);
+
+        // Adicionar intent recebendo as informações do usuário
+
+        nomeUsuario.setText("Nome exemplo");
+        emailUsuario.setText("EmailExemplo123@gmail.com");
+
     }
 }
